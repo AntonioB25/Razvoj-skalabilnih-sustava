0b5f7c01450f43dd226996ea9c7d24003b144b14
Antonio Bukovac <antoniobukovac10@gmail.com>
2023-04-04 21:36:57 +0200

    added swagger

---
+++ src/main/java/com/fer/rss/config/SpringdocConfig.java
@@ -0,0 +1,15 @@
+package com.fer.rss.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition
+@Configuration
+public class SpringdocConfig {
+
+    public OpenAPI baseOpenApi(){
+        return new OpenAPI().info(new Info().title("Spring Doc").version("1.0.0").description("Spring doc"));
+    }
+}
